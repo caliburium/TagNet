3264fceec3ad5eb354343c6b1a2769aa9854fca6
asahi <asahi@dankook.ac.kr>
2025-11-19 23:08:54 +0900

    11.19

--- [CNN]3MNIST.py
+++ [CNN]3MNIST.py
@@ -2,37 +2,32 @@ import argparse
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.optim.lr_scheduler import CosineAnnealingLR, ConstantLR, SequentialLR, LinearLR
 import wandb
+from thop import profile
 from dataloader.data_loader import data_loader
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 class SharedMLP(nn.Module):
-    def __init__(self, num_classes=10, hidden_size=1152, partitioned_size=384):
+    def __init__(self, num_classes=10, partitioned_size=384):
         super(SharedMLP, self).__init__()
         self.num_classes = num_classes
-        self.hidden_size = hidden_size
-        self.input_dim = 1 * 28 * 28
         # self.input_dim = 16 * 7 * 7
 
-        self.cnn = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=16, kernel_size=3, stride=2, padding=1),
+        self.features = nn.Sequential(
+            nn.Conv2d(1, 16, 3, 2, 1),
             nn.BatchNorm2d(16),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
-            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1),
+            nn.MaxPool2d(2, 2, 0),
+            nn.Conv2d(16, 32, 3, 1, 1),
             nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
-            nn.Flatten(),
-            nn.Linear(32 * 3 * 3, hidden_size),
-            nn.BatchNorm1d(hidden_size),
             nn.ReLU(),
         )
 
         self.classifier = nn.Sequential(
-            nn.Linear(hidden_size, partitioned_size),
+            nn.Linear(32 * 7 * 7, partitioned_size),
             nn.ReLU(),
             nn.Linear(partitioned_size, num_classes)
         )
@@ -47,11 +42,11 @@ class SharedMLP(nn.Module):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--epoch', type=int, default=300)
+    parser.add_argument('--epoch', type=int, default=1000)
+    parser.add_argument('--anneal_epochs', type=int, default=150)
     parser.add_argument('--batch_size', type=int, default=500)
     parser.add_argument('--lr', type=float, default=1e-6)
-    parser.add_argument('--hidden_size', type=int, default=1152)
-    parser.add_argument('--part_size', type=int, default=384)
+    parser.add_argument('--part_size', type=int, default=512)
     parser.add_argument('--momentum', type=float, default=0.90)
     parser.add_argument('--opt_decay', type=float, default=1e-6)
 
@@ -61,10 +56,7 @@ def main():
     wandb.init(entity="hails",
                project="TagNet - 3MNIST",
                config=args.__dict__,
-               name="[CNN+Disc]3MNIST_" + str(args.hidden_size)
-                    + "_lr:" + str(args.lr)
-                    + "_Batch:" + str(args.batch_size)
-                    + "_epoch:" + str(args.epoch)
+               name="[CNN]3MNIST_" + str(args.part_size)
                )
 
     mnist_loader, mnist_loader_test = data_loader('MNIST', args.batch_size)
@@ -75,14 +67,45 @@ def main():
 
     print("Data load complete, start training")
 
-    model = SharedMLP(num_classes=10,
-                      hidden_size=args.hidden_size,
-                      partitioned_size=args.part_size).to(device)
+    model = SharedMLP(num_classes=10, partitioned_size=args.part_size).to(device)
+
+    dummy_input = torch.randn(1, 1, 28, 28).to(device)
+    flops_train, thop_params = profile(model, inputs=(dummy_input,), verbose=False)
+
+    trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    params_features = sum(p.numel() for p in model.features.parameters())
+    params_classifier = sum(p.numel() for p in model.classifier.parameters())
+    inference_params = params_features + params_classifier
+
+    print(f"Model Parameters (Trainable): {trainable_params / 1e3:.3f}K")
+    print(f"Model Parameters (Inference Active): {inference_params / 1e3:.3f}K")
+    print(f"Model FLOPs (Train): {flops_train} FLOPs")
+    print(f"Model FLOPs (Inference): {flops_train} FLOPs")
+
+    wandb.log({
+        "Parameters/FLOPs Train": flops_train,
+        "Parameters/FLOPs Inference": flops_train,
+        "Parameters/Trainable Params (K)": trainable_params / 1e3,
+        "Parameters/Inference Params (K)": inference_params / 1e3
+    }, step=0)
+
     # optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.opt_decay)
-    optimizer_f = optim.Adam(model.cnn.parameters(), lr=args.lr)
-    optimizer_c = optim.Adam(model.classifier.parameters(), lr=args.lr)
+    optimizer_c = optim.Adam(model.classifier.parameters(), lr=args.lr, weight_decay=args.opt_decay)
+    optimizer_f = optim.Adam(model.features.parameters(), lr=args.lr, weight_decay=args.opt_decay)
+
+    scheduler_cos_f = CosineAnnealingLR(optimizer_f, T_max=args.anneal_epochs, eta_min=1e-15)
+    scheduler_const_f = ConstantLR(optimizer_f, factor=1e-9, total_iters=args.epoch)
+    scheduler_f = SequentialLR(optimizer_f,
+                               schedulers=[scheduler_cos_f, scheduler_const_f],
+                               milestones=[args.anneal_epochs])
+
+    scheduler_warmup_c = LinearLR(optimizer_c, start_factor=1e-15, end_factor=1.0, total_iters=args.anneal_epochs)
+    scheduler_const_c = ConstantLR(optimizer_c, factor=1.0, total_iters=args.epoch)
+    scheduler_c = SequentialLR(optimizer_c,
+                                schedulers=[scheduler_warmup_c, scheduler_const_c],
+                                milestones=[args.anneal_epochs])
+
     criterion = nn.CrossEntropyLoss()
-    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer_f, T_max=num_epochs, eta_min=0.0)
     best_test_acc = 0.0
 
     for epoch in range(num_epochs):
@@ -147,8 +170,6 @@ def main():
             samples_kmnist += kmnist_labels.size(0)
             samples_fmnist += fmnist_labels.size(0)
 
-        scheduler.step()
-
         avg_loss_mnist = total_loss_mnist / len_dataloader
         avg_loss_kmnist = total_loss_kmnist / len_dataloader
         avg_loss_fmnist = total_loss_fmnist / len_dataloader
@@ -162,7 +183,12 @@ def main():
         print(f'  Train KMNIST | Acc: {acc_kmnist:.3f}%')
         print(f'  Train FMNIST | Acc: {acc_fmnist:.3f}%')
 
+        current_lr_f = scheduler_f.get_last_lr()[0]
+        current_lr_c = scheduler_c.get_last_lr()[0]
+
         wandb.log({
+            'Parameters/LR_feature': current_lr_f,
+            'Parameters/LR_classifier': current_lr_c,
             'Train Loss/Label MNIST': avg_loss_mnist,
             'Train Loss/Label KMNIST': avg_loss_kmnist,
             'Train Loss/Label FMNIST': avg_loss_fmnist,
@@ -171,6 +197,8 @@ def main():
             'Train/Acc Label FMNIST': acc_fmnist,
         }, step=epoch + 1)
 
+        scheduler_c.step()
+        scheduler_f.step()
         model.eval()
 
         def tester(loader):
